8ae275b3527907585f8474ae929cd5fa3def2a42
amirghadiri1987 <144431373+amirghadiri1987@users.noreply.github.com>
2025-01-23 12:50:58 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
+
+# Set the directory to save files
+UPLOAD_FOLDER = '/ServerUpload/1001/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
-	
-# @app.route("/check")
-# def check_File():
-# 	pass
 
-# def upload_file():
-# 	pass
+@app.route('/upload_csv', methods=['POST'])
+def upload_csv():
+    if 'file' not in request.files:
+        return jsonify({'status': 'fail', 'message': 'No file part'}), 400
+    
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'status': 'fail', 'message': 'No selected file'}), 400
 
-# de upload_data():
-# 	pass
-
+    # Save the file
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+    return jsonify({'status': 'success', 'message': 'File uploaded successfully'}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
